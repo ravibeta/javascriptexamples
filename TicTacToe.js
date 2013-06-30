d3563e2307c989a4b5f97cac26a98ea0c438ac43
Ravi Rajamani <ravibeta@hotmail.com>
2013-06-30 15:17:34 -0700

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,41 +1,29 @@
 function GetWinner() {
     var ret = "click me";
-
-    // check horizontals
-    for (var i = 0; i < 3; i = i + 1) {
-        if (getButton(i * 3 + 0).value != "click me" && getButton(i * 3 + 0).value == getButton(i * 3 + 1).value && getButton(i * 3 + 1) == getButton(i * 3 + 2)) return getButton(i * 3 + 0).value;
-    }
-
-    // check verticals
-    for (var j = 0; j < 3; j = j + 1) {
-        if (getButton(i * 3 + 0).value != "click me" && getButton(0 * 3 + j).value == getButton(1 * 3 + j).value && getButton(1 * 3 + j).value == getButton(2 * 3 + j).value) return getButton(0 * 3 + j).value;
-    }
-
-    // check diagonals
-    if (getButton(i * 3 + 0).value != "click me" && getButton(0 * 3 + 0).value == getButton(1 * 3 + 1).value && getButton(1 * 3 + 1).value == getButton(2 * 3 + 2).value) return getButton(0 * 3 + 0).value;
-    if (getButton(i * 3 + 0).value != "click me" && getButton(0 * 3 + 2).value == getButton(1 * 3 + 1).value && getButton(1 * 3 + 1).value == getButton(2 * 3 + 0).value) return getButton(0 * 3 + 2).value;
     return ret;
 }
 
 function Play(pos) {
     var userForm = document.forms[0];
-    var i = pos / 3;
-    var j = pos % 3;
+    var i =  Math.floor(pos / 3);
+    var j = Math.floor(pos % 3);
     var id = "Button" + pos;
     var source = document.getElementById(id);
-    source.value = "    X   ";
+    source.value = "    X    ";
     source.disabled = true;
     var winner = GetWinner();
     if (winner == "click me") {
         var newpos = GetNextMove(pos);
+        if (newpos == -1) return;
         id = "Button" + newpos;
         var target = document.getElementById(id);
-        target.value = "    O   ";
+        target.value = "    O    ";
         target.disabled = true;
     }
 
-    if (winner == "    X   " || winner == "    O   ")
+    if (winner == "    X    " || winner == "    O    "){
         Set();
+        }
 }
 
 function Set() {
@@ -45,7 +33,7 @@ function Set() {
             document.getElementById(id).disabled = true;
         }
     }
-    var actionItem = document.getElementById("Button10")
+    var actionItem = document.getElementById("Button10");
     actionItem.disabled = false;
     actionItem.value = "Reset";
 }
@@ -59,40 +47,41 @@ function Reset() {
             target.value = "click me";
         }
     }
-    var actionItem = document.getElementById("Button10")
+    var actionItem = document.getElementById("Button10");
     actionItem.disabled = true;
     actionItem.value = "Play";
 }
 
 function GetNextMove(pos) {
-    var i = pos / 3;
-    var j = pos % 3;
-    var value = getButton(i, j).value;
-    if (i - 1 > 0 && j - 1 > 0 && getButton(i - 1, j - 1).Value == value) {
-        if (i + 1 < 3 && j + 1 < 3 && getButton(i + 1, j + 1).value == "click me") return (i + 1) * 3 + (j + 1);
-        if (i - 2 > 0 && j - 2 > 0 && getButton(i - 2, j - 2).value == "click me") return (i - 2) * 3 + (j - 2);
+    var i = Math.floor(pos / 3);
+    var j = Math.floor(pos % 3);
+    var value = document.getElementById("Button" + (i, j)).value;
+    if (i - 1 >= 0 && j - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == value) {
+        if (i + 1 < 3 && j + 1 < 3 && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == "click me") { return ((i + 1) * 3 + (j + 1)); }
+        if (i - 2 >= 0 && j - 2 >= 0 && document.getElementById("Button" + ((i - 2) * 3 + j - 2)).value == "click me") { return ((i - 2) * 3 + (j - 2)); }
     }
-    if (j - 1 > 0 && getButton(i, j - 1).value == value) {
-        if (j + 1 < 3 && getButton(i, j + 1).value == "click me") return (i * 3 + j + 1);
-        if (j - 2 > 0 && getButton(i, j - 2).value == "click me") return (i * 3 + j - 2);
+    if (j - 1 >= 0 && document.getElementById("Button" + ((i * 3) + j - 1)).value == value) {
+        if (j + 1 < 3 && document.getElementById("Button" + ((i * 3) + j + 1)).value == "click me") { return (i * 3 + j + 1); }
+        if (j - 2 >= 0 && document.getElementById("Button" + ((i * 3) + j - 2)).value == "click me") { return (i * 3 + j - 2); }
     }
-    if (i - 1 > 0 && j + 1 < 3 && getButton(i - 1, j + 1).value == value) {
-        if (i + 1 < 3 && j - 2 > 0 && getButton(i + 1, j - 2).value == "click me") return (i + 1) * 3 + j - 2;
-        if (i - 2 > 0 && j + 2 < 3 && getButton(i - 2, j + 2).value == "click me") return (i - 2) * 3 + j + 2;
+    if (i - 1 >= 0  && j + 1 < 3 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == value) {
+        if (i + 1 < 3 && j - 1 > 0 && document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value == "click me") { return ((i + 1) * 3 + (j - 1)); }
+        if (i - 2 >= 0 && j + 2 < 3 && document.getElementById("Button" + ((i - 2) * 3 + j + 2)).value == "click me") { return ((i - 2) * 3 + (j + 2)); }
     }
-    if (i - 1 > 0 && getButton(i - 1, j).value == value) {
-        if (i + 1 < 3 && getButton(i + 1, j).value == "click me") return (i * 3 + j + 1);
-        if (i - 2 > 0 && getButton(i - 2, j).value == "click me") return (i - 2) * 3 + j;
+    if (i - 1 >= 0  && document.getElementById("Button" + ((i - 1) * 3 + j)).value == value) {
+        if (i + 1 < 3 && document.getElementById("Button" + ((i + 1) * 3 + j)).value == "click me") { return ((i + 1) * 3 + j); }
+        if (i - 2 >= 0 && document.getElementById("Button" + ((i - 2) * 3 + j)).value == "click me") { return ((i - 2) * 3 + j); }
     }
-    for (var k = 0; k < 9; k = k + 1) {
-        if (getButton(k).value == "click me") return k;
+    for (var k = 0; k < 9; k = k + 1){
+        if (document.getElementById("Button" + (k)).value == "click me") { return k; }
     }
+    return -1;
 }
 
 function getButton(pos) {
     return document.getElementById("Button" + pos);
 }
 
-function getButton(i, j) {
-    return getButton(i * 3 + j);
+function getButton(i,j) {
+    return document.getElementById("Button" + (i * 3 + j));
 }
