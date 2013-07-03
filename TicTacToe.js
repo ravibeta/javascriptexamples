2aafd0629a68df5612076a7db0de49b929720341
Ravi Rajamani <ravibeta@hotmail.com>
2013-07-03 16:36:19 -0700

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -28,7 +28,7 @@ function Play(pos) {
     source.disabled = true;
     var winner = GetWinner();
     if (winner == "click me") {
-        var newpos = GetNextMove1(pos);
+        var newpos = GetNextMove("    O    ");
         if (newpos == -1) return;
         id = "Button" + newpos;
         var target = document.getElementById(id);
@@ -188,35 +188,42 @@ function GetNextMove(c)
 function ThreeInARow(i, j, c)
 {
     var gameOver = false;
-            
     if (i - 1 >= 0 && j - 1 >= 0
-        && i + 1 <= 2 && j + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value)
+        && i + 1 <= 2 && j + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value== c && c == document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value) { gameOver = true;}
+        if (document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == c && c == document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value) {
+            gameOver = true;
+            }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (i - 1 >= 0
-        && i + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j)).value == document.getElementById("Button" + ((i + 1) * 3 + j)).value)
+        && i + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j)).value != "click me"  && document.getElementById("Button" + ((i - 1) * 3 + j)).value == document.getElementById("Button" + ((i + 1) * 3 + j)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i + 1) * 3 + j)).value) { gameOver = true; }
+        if (document.getElementById("Button" + ((i - 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i + 1) * 3 + j)).value) {
+            gameOver = true;
+            }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (i - 1 >= 0 && j + 1 <= 2
-        && i + 1 <= 2 && j - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i + 1) * 3 + j - 1).value))
+        && i + 1 <= 2 && j - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value) { gameOver = true;}
+        if (document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (j - 1 >= 0
-        && j + 1 <= 2 && document.getElementById("Button" + (i * 3 + j - 1)).value == document.getElementById("Button" + (i * 3 + j + 1)).value)
+        && j + 1 <= 2 && document.getElementById("Button" + (i * 3 + j - 1)).value != "click me" && document.getElementById("Button" + (i * 3 + j - 1)).value == document.getElementById("Button" + (i * 3 + j + 1)).value)
     {
-        if (document.getElementById("Button" + (i * 3 + j - 1)).value == c && c == document.getElementById("Button" + (i * 3 + j + 1)).value) { gameOver = true; }
+        if (document.getElementById("Button" + (i * 3 + j - 1)).value == c && c == document.getElementById("Button" + (i * 3 + j + 1)).value) {
+            gameOver = true;
+            }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
@@ -226,113 +233,125 @@ function ThreeInARow(i, j, c)
 function SkipNeighbour3InARow( i,  j,  c)
 {
     var gameOver = false;
-            
     if (i - 1 >= 0 && j - 1 >= 0
-        && i - 2 >= 0 && j - 2 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == document.getElementById("Button" + ((i - 2) * 3 + j - 2)).value)
+        && i - 2 >= 0 && j - 2 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == document.getElementById("Button" + ((i - 2) * 3 + j - 2)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j - 2)).value) { gameOver = true; }
+        if (document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j - 2)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (i - 1 >= 0
-        && i - 2 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j)).value == document.getElementById("Button" + ((i - 2) * 3 + j)).value)
+        && i - 2 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j)).value == document.getElementById("Button" + ((i - 2) * 3 + j)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j)).value) { gameOver = true; }
+        if (document.getElementById("Button" + ((i - 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
+
     }
 
     if (i - 1 >= 0 && j + 1 <= 2
-        && i - 2 >= 0 && j + 2 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i - 2) * 3 + j + 2)).value)
+        && i - 2 >= 0 && j + 2 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i - 2) * 3 + j + 2)).value)
     {
-        if (document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j + 2)).value) { gameOver = true;}
+        if (document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i - 2) * 3 + j + 2)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (j + 1 <= 2
-        && j + 2 <= 2 && document.getElementById("Button" + (i * 3 + j + 1)).value == document.getElementById("Button" + (i * 3 + j + 2)).value)
+        && j + 2 <= 2 && document.getElementById("Button" + (i * 3 + j + 1)).value != "click me" && document.getElementById("Button" + (i * 3 + j + 1)).value == document.getElementById("Button" + (i * 3 + j + 2)).value)
     {
-        if (document.getElementById("Button" + (i * 3 + j + 1)).value == c && c == document.getElementById("Button" + (i * 3 + j + 2)).value) { gameOver = true;} 
+        if (document.getElementById("Button" + (i * 3 + j + 1)).value == c && c == document.getElementById("Button" + (i * 3 + j + 2)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (i + 1 <= 2 && j + 1 <= 2
-                && i + 2 <= 2 && j + 2 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i + 2) * 3 + j + 2)).value) 
+                && i + 2 <= 2 && j + 2 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value != "click me" && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == document.getElementById("Button" + ((i + 2) * 3 + j + 2)).value) 
     {
-        if (document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i + 2) * j + 2)).value) { gameOver = true; }
+        if (document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == c && c == document.getElementById("Button" + ((i + 2) * j + 2)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (i + 1 <= 2
-        && i + 2 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j)).value == document.getElementById("Button" + ((i + 2) * 3 + j)).value)
+        && i + 2 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j)).value != "click me" && document.getElementById("Button" + ((i + 1) * 3 + j)).value == document.getElementById("Button" + ((i + 2) * 3 + j)).value)
     {
-        if (document.getElementById("Button" + ((i + 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i + 2) * 3 + j)).value) { gameOver = true; }
+        if (document.getElementById("Button" + ((i + 1) * 3 + j)).value == c && c == document.getElementById("Button" + ((i + 2) * 3 + j)).value) {
+            gameOver = true;
+            }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
     if (j - 1 >= 0
-        && j - 2 >= 0 && document.getElementById("Button" + (i * 3 + j - 1)).value == document.getElementById("Button" + (i * 3 + j - 2)).value)
+        && j - 2 >= 0 && document.getElementById("Button" + (i * 3 + j - 1)).value != "click me" && document.getElementById("Button" + (i * 3 + j - 1)).value == document.getElementById("Button" + (i * 3 + j - 2)).value)
     {
-        if (document.getElementById("Button" + (i * 3 + j - 1)).value == c && c == document.getElementById("Button" + (i * 3 + j - 2)).value) { gameOver = true; }
+        if (document.getElementById("Button" + (i * 3 + j - 1)).value == c && c == document.getElementById("Button" + (i * 3 + j - 2)).value) {
+            gameOver = true;
+        }
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
-
     return -1;
 }
 
 function TwoInARow(i, j, c)
 {
     var gameOver = false;
-            
-    if (i - 1 >= 0 && j - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == c)
+    if (i - 1 >= 0 && j - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j - 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (i - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j)).value == c)
+    if (i - 1 >= 0 && document.getElementById("Button" + ((i - 1) * 3 + j)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (i - 1 >= 0 && j + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c)
+    if (i - 1 >= 0 && j + 1 <= 2 && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value != "click me" && document.getElementById("Button" + ((i - 1) * 3 + j + 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (j + 1 <= 2 && document.getElementById("Button" + ((i * 3) + j + 1)).value == c)
+    if (j + 1 <= 2 && document.getElementById("Button" + ((i * 3) + j + 1)).value != "click me" && document.getElementById("Button" + ((i * 3) + j + 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (i + 1 <= 2 && j + 1 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == c)
+    if (i + 1 <= 2 && j + 1 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value != "click me" && document.getElementById("Button" + ((i + 1) * 3 + j + 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (i + 1 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j)).value == c)
+    if (i + 1 <= 2 && document.getElementById("Button" + ((i + 1) * 3 + j)).value != "click me" && document.getElementById("Button" + ((i + 1) * 3 + j)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (i + 1 <= 2 && j - 1 >= 0 && document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value == c)
+    if (i + 1 <= 2 && j - 1 >= 0 && document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value != "click me" && document.getElementById("Button" + ((i + 1) * 3 + j - 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
     }
 
-    if (j - 1 >= 0 && document.getElementById("Button" + (i * 3 + j - 1)).value == c)
+    if (j - 1 >= 0 && document.getElementById("Button" + (i * 3 + j - 1)).value != "click me" && document.getElementById("Button" + (i * 3 + j - 1)).value == c)
     {
         document.getElementById("Button" + (i * 3 + j)).value = c;
         return i * 3 + j;
