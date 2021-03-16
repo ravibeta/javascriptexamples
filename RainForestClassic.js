b4e5aca4d34d997cc161010332645ade4c6d19fa
Ravi Rajamani <ravi.rajamani@emc.com>
2021-03-15 21:54:18 -0700

    RainForestClassicTest: Test that search on Ravi's blog work

---
+++ RainForestClassic.js
@@ -0,0 +1,6 @@
+Manual Test Instructions
+
+tester starts at https://bing.com/
+1
+Fill search bar with oauth site://ravinote.blogs... . Click left mouse button on search button.
+Can you observe that search result is visible?
